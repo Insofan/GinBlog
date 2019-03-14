dbb236a2d559285b49e72965b120d77c9aa8384d
Insomnia <282132315@qq.com>
2019-03-14 22:11:23 +0800

    sync

---
+++ service/pkg/setting/setting.go
@@ -0,0 +1,16 @@
+/**
+ * Created with Goland.
+ * Description: 
+ * User: Insomnia
+ * Date: 2019-03-14
+ * Time: 下午10:05
+ */
+/*
+	Package setting: The config of service
+*/
+package setting
+
+func init() {
+	//viper.Setter
+}
+
