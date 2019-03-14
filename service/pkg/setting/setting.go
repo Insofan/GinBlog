2c5453bbe78c77e9fc4c616b38ce49d552f9871d
Insomnia <282132315@qq.com>
2019-03-14 23:10:12 +0800

    service add setting file

--- service/pkg/setting/setting.go
+++ service/pkg/setting/setting.go
@@ -1,16 +1,33 @@
-/**
- * Created with Goland.
- * Description: 
- * User: Insomnia
- * Date: 2019-03-14
- * Time: 下午10:05
- */
 /*
-	Package setting: The config of service
+Package setting the config of service
 */
 package setting
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var v  *viper.Viper
 func init() {
-	//viper.Setter
+	v = viper.New()
+	v.SetConfigName("config")
+	v.SetConfigType("json")
+	v.AddConfigPath("conf")
+
+	// Handle errors reading the config file
+	err := v.ReadInConfig()
+
+	if err != nil {
+		panic(fmt.Errorf("FATAL ERROR CONFIG FILE"))
+	}
+}
+
+//LoadServer server config
+func LoadServer() error {
+	str := v.GetString("host.address")
+	port := v.GetInt64("host.port")
+	fmt.Println(str, port)
+	return nil
 }
 
